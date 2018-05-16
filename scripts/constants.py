27a6578f15dbbf0759f8ae12957231432d0345e6
Joseph Fernandez <joseph.fdez21@gmail.com>
2018-05-16 13:30:42 +0100

    Different scripts and librarirs

---
+++ scripts/constants.py
@@ -0,0 +1,7 @@
+# Physical, mathematical and unit conversion constats
+
+GG   = 6.67408E-11            # SI units
+cc   = 2.99792E8              # m/s
+Msun = 1.98855E30             # kg
+au   = 149597870700           # Astronomical units in metre
+pc   = 3.0857e16              # Parsec in metres
